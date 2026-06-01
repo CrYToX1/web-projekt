84e52800b734702974b74b1cccd0519c1ede41f0
Zaytryn <janotik598@gmail.com>
2026-06-01 07:43:25 +0000

    feat: vice faktu

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function showFact() {
+
+    const facts = [
+        "Rainbow Six Siege vyšlo v roce 2015.",
+        "Ve hře je více než 70 operátorů.",
+        "Každá mapa umožňuje destrukci zdí, podlah a stropů.",
+        "Hra je součástí série Tom Clancy's Rainbow Six.",
+        "Siege je jednou z nejúspěšnějších taktických FPS her."
+    ];
+
+    const randomFact =
+        facts[Math.floor(Math.random() * facts.length)];
+
     document.getElementById("fact").textContent =
-        "Rainbow Six Siege vyšlo v roce 2015 a stále patří mezi nejhranější taktické FPS hry.";
+        randomFact;
 }
